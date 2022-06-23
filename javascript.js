ce29b2876d5754aff43d7730d14f9363a8d29a74
Munga Maseru <eclick254@icloud.com>
2022-06-23 19:20:56 +0300

    Add pseudocode for function that plays a single round.

--- javascript.js
+++ javascript.js
@@ -11,6 +11,15 @@ Function that randomly returns either 'Rock', 'Paper' or 'Scissors'
         return computerResult;
     };
 
-
+/*
+Function that plays a single round of Rock Paper Scissors
+*/
+    // initialize function; singleRoundPlay that takes two arguments: playerSelection & computerSelection
+    // prompt player to select Rock, Paper, or Scissors
+    // store player choice in variable playerSelection
+    // call function computerPlay()
+    // store computerResult in variable computerSelection
+    // compare playerSelection and computerSelection and store winner of the round in variable roundWinner
+    // return roundWinner
 
     
