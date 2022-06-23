3ddb3688258da6a7f199aeef757066590b4d7f8f
Munga Maseru <eclick254@icloud.com>
2022-06-23 22:58:49 +0300

    Modify comments on singleRoundPlay function for clarity.

--- javascript.js
+++ javascript.js
@@ -22,44 +22,53 @@ Function that plays a single round of Rock Paper Scissors
         // call function computerPlay()
         // store computerResult in variable computerSelection
         computerSelection = computerPlay();
-        // compare playerSelection and computerSelection and store winner of the round in variable roundWinner
+        // compare playerSelection and computerSelection
         if (playerSelection === "rock" && computerSelection === "Paper") {
             alert("You selected " + playerSelection.charAt(0).toUpperCase() + playerSelection.slice(1));
             alert("Computer selected " + computerSelection);
             alert("You Lose! Paper beats Rock!!");
+            // store winner of the round in variable roundWinner
             roundWinner = "Computer";
         } else if (playerSelection === "paper" && computerSelection === "Scissors") {
             alert("You selected " + playerSelection.charAt(0).toUpperCase() + playerSelection.slice(1));
             alert("Computer selected " + computerSelection);
             alert("You Lose! Scissors beat Paper!!");
+            // store winner of the round in variable roundWinner
             roundWinner = "Computer";
         } else if (playerSelection === "scissors" && computerSelection === "Rock") {
             alert("You selected " + playerSelection.charAt(0).toUpperCase() + playerSelection.slice(1));
             alert("Computer selected " + computerSelection);
             alert("You Lose! Rock beats Scissors!!");
+            // store winner of the round in variable roundWinner
             roundWinner = "Computer";
         } else if (playerSelection === "rock" && computerSelection === "Scissors") {
             alert("You selected " + playerSelection.charAt(0).toUpperCase() + playerSelection.slice(1));
             alert("Computer selected " + computerSelection);
             alert("You Win! Rock beats Scissors!!");
+            // store winner of the round in variable roundWinner
             roundWinner = "Player";
         } else if (playerSelection === "paper" && computerSelection === "Rock") {
             alert("You selected " + playerSelection.charAt(0).toUpperCase() + playerSelection.slice(1));
             alert("Computer selected " + computerSelection);
             alert("You Win! Paper beats Rock!!");
+            // store winner of the round in variable roundWinner
             roundWinner = "Player";
         } else if (playerSelection === "scissors" && computerSelection === "Paper") {
             alert("You selected " + playerSelection.charAt(0).toUpperCase() + playerSelection.slice(1));
             alert("Computer selected " + computerSelection);
             alert("You Win! Scissors beat Paper!!");
+            // store winner of the round in variable roundWinner
             roundWinner = "Player";
         } else if (playerSelection === computerSelection.toLowerCase()) {
             alert("You selected " + playerSelection.charAt(0).toUpperCase() + playerSelection.slice(1));
             alert("Computer selected " + computerSelection);
             alert("It's a tie!");
+            // store winner of the round in variable roundWinner
             roundWinner = null;
         } else {
+            // condition in case player does not enter a valid Rock Paper Scissors selection
             alert("Unknown selection! \nPlease select either 'Rock', 'Paper', or 'Scissors.'");
+            // store winner of the round in variable roundWinner
             roundWinner = undefined;
         }
         // return roundWinner
