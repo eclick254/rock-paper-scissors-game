428afd511a65f7bf9772e4129d612bc5d0495379
Munga Maseru <eclick254@icloud.com>
2022-06-23 19:19:10 +0300

    Modify pseudocode for function computerPlay for better clarity.

--- javascript.js
+++ javascript.js
@@ -1,14 +1,16 @@
-// Function that randomly returns either 'Rock', 'Paper' or 'Scissors'
-    // initialize function: computerPlay
-    function computerPlay(str) {
-        // create variable array with game options 
-        gameOptions = ['Rock', 'Paper', 'Scissors'];
-        // randomly set 'Rock', 'Paper' or 'Scissors' as gameResult
-        gameResult = gameOptions[Math.floor(Math.random()*gameOptions.length)];
-        // return value of gameResult
-        return gameResult;
+/*
+Function that randomly returns either 'Rock', 'Paper' or 'Scissors'
+*/
+    // initialize function; computerPlay
+    function computerPlay() {
+        // create array of Rock Paper Scissors options and store as computerOptions 
+        computerOptions = ['Rock', 'Paper', 'Scissors'];
+        // randomly select a string from computerOptions and store as computerResult
+        computerResult = computerOptions[Math.floor(Math.random()*computerOptions.length)];
+        // return option randomly selected as computerResult
+        return computerResult;
     };
 
-    
+
 
     
