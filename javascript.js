9150714e65290dc9a5302cc1acaf4772450bc3ad
Munga Maseru <eclick254@icloud.com>
2022-06-23 21:29:00 +0300

    Add code for singleRoundPlay function.

--- javascript.js
+++ javascript.js
@@ -15,11 +15,45 @@ Function that randomly returns either 'Rock', 'Paper' or 'Scissors'
 Function that plays a single round of Rock Paper Scissors
 */
     // initialize function; singleRoundPlay that takes two arguments: playerSelection & computerSelection
-    // prompt player to select Rock, Paper, or Scissors
-    // store player choice in variable playerSelection
-    // call function computerPlay()
-    // store computerResult in variable computerSelection
-    // compare playerSelection and computerSelection and store winner of the round in variable roundWinner
-    // return roundWinner
+    function singleRoundPlay(playerSelection, computerSelection) {
+        // prompt player to select Rock, Paper, or Scissors
+        // store player choice in variable playerSelection
+        playerSelection = prompt("Let's play Rock Paper Scissors. 1..2..3 Go! ");
+        // call function computerPlay()
+        // store computerResult in variable computerSelection
+        computerSelection = computerPlay();
+        // compare playerSelection and computerSelection and store winner of the round in variable roundWinner
+        if (playerSelection === "Rock" && computerSelection === "Paper") {
+            alert("Computer selected " + computerSelection);
+            alert("You Lose! Paper beats Rock!!");
+            roundWinner = "Computer";
+        } else if (playerSelection === "Paper" && computerSelection === "Scissors") {
+            alert("Computer selected " + computerSelection);
+            alert("You Lose! Scissors beat Paper!!");
+            roundWinner = "Computer";
+        } else if (playerSelection === "Scissors" && computerSelection === "Rock") {
+            alert("Computer selected " + computerSelection);
+            alert("You Lose! Rock beats Scissors!!");
+            roundWinner = "Computer";
+        } else if (playerSelection === "Rock" && computerSelection === "Scissors") {
+            alert("Computer selected " + computerSelection);
+            alert("You Win! Rock beats Scissors!!");
+            roundWinner = "Player";
+        } else if (playerSelection === "Paper" && computerSelection === "Rock") {
+            alert("Computer selected " + computerSelection);
+            alert("You Win! Paper beats Rock!!");
+            roundWinner = "Player";
+        } else if (playerSelection === "Scissors" && computerSelection === "Paper") {
+            alert("Computer selected " + computerSelection);
+            alert("You Win! Scissors beat Paper!!");
+            roundWinner = "Player";
+        } else {
+            alert("Computer selected " + computerSelection);
+            alert("It's a tie!");
+            roundWinner = null;
+        }
+        // return roundWinner
+        return(roundWinner); 
+        }
 
     
